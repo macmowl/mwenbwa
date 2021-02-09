7f987a4bc6fa132562e35f4b519f7c2bdc6e4ca1
Alan Massaro <alan.massar@gmail.com>
2021-02-09 15:14:14 +0100

    add bulma

--- src/client/app.js
+++ src/client/app.js
@@ -8,6 +8,7 @@
 
 import * as React from "react";
 import ReactDOM from "react-dom";
+import "./style.scss";
 
 import HelloWorld from "./components/hello";
 
--- webpack.config.js
+++ webpack.config.js
@@ -73,6 +73,10 @@ module.exports = env => {
                     ],
                 },
                 {
+                    test: /\.s[ac]ss$/i,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                {
                     test: /\.js$/,
                     exclude: [/node_modules/],
                     use: [
