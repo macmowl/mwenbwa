8b1242987c18514b015a67a3bf9d16c987b1f118
Alan Massaro <alan.massar@gmail.com>
2021-02-11 17:05:21 +0100

    map displayed

--- src/client/components/gamePage/leaflet-map.js
+++ src/client/components/gamePage/leaflet-map.js
@@ -1,5 +1,28 @@
 import React from "react";
+import {MapContainer, TileLayer} from "react-leaflet";
+import treeData from "../../../../data/bintrees.json";
 
-const LeafletMap = () => <h2>{"essai"}</h2>;
+const LeafletMap = () => {
+    // const [activeTree, setAcativeTree] = useState(null);
+    console.log(treeData);
+    return (
+        <MapContainer center={[50.64497, 5.57333]} zoom={16}>
+            <TileLayer
+                attribution={
+                    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                }
+                url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+            />
+            {/* {treeData.map(tree => (
+                    <Marker position={[
+                        tree.location.coordinates[1],
+                        tree.location.coordinates[0]
+                    ]}>
+                    
+                    </Marker>
+                ))} */}
+        </MapContainer>
+    );
+};
 
 export default LeafletMap;
