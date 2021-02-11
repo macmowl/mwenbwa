d2c880c590513e21b0d0a0b26e6120f45f21879b
User <rachida.haddouch.webdev@gmail.com>
2021-02-11 16:44:56 +0100

    login register

--- src/client/components/connectPage/login.js
+++ src/client/components/connectPage/login.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 const Login = () => (
-    <div className={"login-content is-half column"}>
-        <h1 className={"title"}>{"Login"}</h1>
-        <h2 className={"subtitle"}>{"Log in to your account"}</h2>
+    <div className={"login-content is-half column "}>
+        <h1 className={"title has-text-grey"}>{"Login"}</h1>
+        <h2 className={"subtitle has-text-grey"}>{"Log in to your account"}</h2>
 
         <div className={"field"}>
-            <label className={"label"}>{"Email"}</label>
+            <label className={"label has-text-grey"}>{"Email"}</label>
             <div className={"control"}>
                 <input
-                    className={"input is-medium is-rounded"}
+                    className={"input has-text-grey is-medium is-rounded"}
                     type={"email"}
                     placeholder={"Enter your email adress"}
                 />
@@ -17,15 +17,22 @@ const Login = () => (
         </div>
 
         <div className={"field"}>
-            <label className={"label"}>{"Password"}</label>
+            <label className={"label has-text-grey has-text-grey"}>
+                {"Password"}
+            </label>
             <div className={"control"}>
                 <input
-                    className={"input is-medium is-rounded"}
+                    className={"input has-text-grey is-medium is-rounded"}
                     type={"password"}
                     placeholder={"**********"}
                 />
             </div>
         </div>
+        <p className={"has-text-centered"}>
+            <a className={"button is-medium is-primary is-rounded"}>
+                {" Login "}
+            </a>
+        </p>
     </div>
 );
 
--- src/client/components/connectPage/sign.js
+++ src/client/components/connectPage/sign.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./login";
 import Register from "./register";
+import Logosmall from "./logo_register-login";
 
 const ConnectPage = () => (
     <div className={"container is-flex"}>
         <Login />
         <Register />
+        <Logosmall />
     </div>
 );
 
