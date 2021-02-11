ee28672bba08dec4c5e8f5c9eef5f404815dbdec
SandrineLe <le.sandrinemt@gmail.com>
2021-02-11 15:06:02 +0100

    adding map

--- src/client/components/gamePage/map.js
+++ src/client/components/gamePage/map.js
@@ -4,13 +4,19 @@ import TreeDetails from "./tree-details";
 import LeafletMap from "./leaflet-map";
 
 const GameMap = () => (
-    <>
-        <LeafletMap />
-        <div className={"Sidebar"}>
-            <Profile />
-            <TreeDetails />
+    <div className={"columns is-gapless is-multiline is-mobile"}>
+        <div className={"column"}>
+            <LeafletMap />
+            {"test"}
         </div>
-    </>
+
+        <div className={"column has-background-primary is-one-fifth"}>
+            <div className={"Sidebar"}>
+                <Profile />
+                <TreeDetails />
+            </div>
+        </div>
+    </div>
 );
 
 export default GameMap;
