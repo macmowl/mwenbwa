e9a776fe056e856ca03987a644814f7e357022d6
SandrineLe <le.sandrinemt@gmail.com>
2021-02-12 09:43:33 +0100

    map.js maj

--- src/client/components/gamePage/profile.js
+++ src/client/components/gamePage/profile.js
@@ -1,14 +1,45 @@
 import React from "react";
 import UserDetails from "./user-details";
-import Button from "./../button";
+
+import {
+    faClock,
+    faSignOutAlt,
+    faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Profile = () => (
     <>
         <UserDetails />
-        <Button />
-        <p>{"Gamelog"}</p>
-        <p>{"ranking"}</p>
-        <p>{"logout"}</p>
+        <div className={"box m-3"}>
+            <div
+                className={
+                    "columns is-gapless is-1-mobile is-0-tablet is-3-desktop is-8-widescreen is-2-fullhd"
+                }>
+                <div className={"column has-text-centered is-size-7"}>
+                    <a className={"has-text-black-ter"}>
+                        <FontAwesomeIcon icon={faClock} />
+                        <span className={"is-block is-small"}>
+                            {"Game log"}
+                        </span>
+                    </a>
+                </div>
+
+                <div className={"column has-text-centered is-size-7"}>
+                    <a className={"has-text-black-ter"}>
+                        <FontAwesomeIcon icon={faUsers} />
+                        <span className={"is-block is-small"}>{"Ranking"}</span>
+                    </a>
+                </div>
+
+                <div className={"column has-text-centered is-size-7"}>
+                    <a className={"has-text-black-ter"}>
+                        <FontAwesomeIcon icon={faSignOutAlt} />
+                        <span className={"is-block is-small"}>{"Logout"}</span>
+                    </a>
+                </div>
+            </div>
+        </div>
     </>
 );
 
