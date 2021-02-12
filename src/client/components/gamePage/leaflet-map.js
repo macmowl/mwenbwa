2728d3196c994fab4951240f8be6474b3cc9c7b4
Alan Massaro <alan.massar@gmail.com>
2021-02-12 12:30:53 +0100

    set random shape for each tree

--- src/client/components/gamePage/leaflet-map.js
+++ src/client/components/gamePage/leaflet-map.js
@@ -1,10 +1,26 @@
 import React from "react";
 import {MapContainer, TileLayer, Marker} from "react-leaflet";
 import treeData from "../../../../data/bintrees.json";
+import {Icon} from "leaflet";
+import {TREE_SHAPES} from "../../core/constants.js";
 
 const LeafletMap = () => {
+    console.log(TREE_SHAPES[1]);
+    const setRndIcon = shape => {
+        const treeIcon = new Icon({
+            iconUrl: TREE_SHAPES[shape],
+            iconSize: [25, 35],
+            iconAnchor: [25, 17],
+        });
+        return treeIcon;
+    };
+
     const treesMarker = treeData.map(tree => (
-        <Marker key={tree._id.$oid} position={tree.location.coordinates} />
+        <Marker
+            key={tree._id.$oid}
+            position={tree.location.coordinates}
+            icon={setRndIcon(tree.shape)}
+        />
     ));
     return (
         <MapContainer center={[50.64497, 5.57333]} zoom={16}>
